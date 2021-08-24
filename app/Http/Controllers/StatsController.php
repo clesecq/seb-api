51d3c2e3329e4977bbdf2d3b23c7c62dfd60270e
Maxime FRIESS <maxime.friess@etu.unistra.fr>
2021-08-24 22:49:51 +0200

    [api] Fixed issues with stats

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\DB;
 
 class StatsController extends Controller
 {
-    private function _most_sold_product() {
+    private function _most_sold_product()
+    {
         $most_sold = 'N/A';
         $most_sold_id = DB::table('product_movement')->groupBy('product_id')->select('product_id')->orderByRaw('sum(count)')->first();
         if ($most_sold_id !== null) {
@@ -22,52 +23,77 @@ class StatsController extends Controller
         return $most_sold;
     }
 
-    private function _coffee_liters() {
+    private function _coffee_liters()
+    {
         $coffees = DB::table('products')->where('name', 'like', '%Café%')->orWhere('name', 'like', '%Expresso%')->orWhere('name', 'like', '%Intenso%')->pluck('id')->toArray();
         $sales = DB::table('sales')->select('movement_id')->groupBy('movement_id')->pluck('movement_id')->toArray();
-        
+
         $count = DB::table('product_movement')->select('count')->whereIn('product_id', $coffees)->whereIn('movement_id', $sales)->sum('count');
         return $count * -0.25;
     }
 
-    private function _product(string $name) {
+    private function _product(string $name)
+    {
         $products = DB::table('products')->where('name', 'like', '%' . $name . '%')->pluck('id')->toArray();
         $sales = DB::table('sales')->select('movement_id')->groupBy('movement_id')->pluck('movement_id')->toArray();
-        
+
         $count = DB::table('product_movement')->select('count')->whereIn('product_id', $products)->whereIn('movement_id', $sales)->sum('count');
         return -$count;
     }
 
-    private function _last_sell_date(string $name) {
+    private function _last_sell_date(string $name)
+    {
         $products = DB::table('products')->where('name', 'like', '%' . $name . '%')->pluck('id')->toArray();
         $sales = DB::table('sales')->select('movement_id')->groupBy('movement_id')->pluck('movement_id')->toArray();
-        
+
         $id = DB::table('product_movement')->select('movement_id')->whereIn('product_id', $products)->whereIn('movement_id', $sales)->orderBy('movement_id', 'desc')->first();
-        return Movement::find($id->movement_id)->created_at;
+
+        if ($id == null) {
+            return 0;
+        } else {
+            return Movement::find($id->movement_id)->created_at;
+        }
     }
 
-    private function _biggest_sale() {
+    private function _biggest_sale()
+    {
         $transactions = DB::table('sales')->select('transaction_id')->pluck('transaction_id')->toArray();
         $sale = DB::table('transactions')->whereIn('id', $transactions)->orderBy('amount', 'desc')->first();
+        if ($sale == null)
+            return 0;
         return $sale->amount;
     }
 
-    private function _latest_restock() {
+    private function _latest_restock()
+    {
         $transactions = DB::table('purchases')->select('transaction_id')->whereNotNull('movement_id')->pluck('transaction_id')->toArray();
-        return DB::table('transactions')->select('created_at')->whereIn('id', $transactions)->orderBy('created_at', 'desc')->first()->created_at;
+        $restock = DB::table('transactions')->select('created_at')->whereIn('id', $transactions)->orderBy('created_at', 'desc')->first();
+        if ($restock == null)
+            return 0;
+        return $restock->created_at;
     }
 
-    private function _most_sales() {
+    private function _most_sales()
+    {
         $transactions = DB::table('sales')->select('transaction_id')->pluck('transaction_id')->toArray();
         $user_id = DB::table('transactions')->groupBy('user_id')->select('user_id')->whereIn('id', $transactions)->orderByRaw('sum(amount)')->first();
+        
+        if ($user_id == null)
+            return "N/A";
+        
         return User::find($user_id->user_id)->firstname;
     }
 
-    private function _price_coca() {
+    private function _price_coca()
+    {
+        if (Product::where('name', 'Coca-Cola')->first() == null)
+            return 0;
+
         return Product::where('name', 'Coca-Cola')->first()->price;
     }
 
-    public function stats(Request $request) {
+    public function stats(Request $request)
+    {
         return [
             'most_sold_product' => $this->_most_sold_product(),
             'sold_coffee_liters' => $this->_coffee_liters(),
--- resources/js/src/resources/Profile.js
+++ resources/js/src/resources/Profile.js
@@ -11,7 +11,7 @@ import SecurityIcon from '@material-ui/icons/Security';
 import VpnKeyIcon from '@material-ui/icons/VpnKey';
 import { spacing } from "@material-ui/system";
 import React, { useState } from 'react';
-import { ArrayField, Button, ChipField, Edit, Labeled, ReferenceArrayField, SaveButton, SimpleForm, SingleFieldList, TextField, TextInput, Toolbar, useAuthProvider, useDeleteWithConfirmController, useNotify, useRecordContext, useRedirect, useRefresh, useTranslate, useUpdateLoading } from 'react-admin';
+import { ArrayField, Button, ChipField, Datagrid, Edit, Labeled, ReferenceArrayField, SaveButton, SimpleForm, SingleFieldList, TextField, TextInput, Toolbar, useAuthProvider, useDeleteWithConfirmController, useNotify, useRecordContext, useRedirect, useRefresh, useTranslate, useUpdateLoading } from 'react-admin';
 import DateField from '../components/DateField';
 const StyledButton = styled(Button)(spacing);
 const StyledGrid = styled(Grid)(spacing);
