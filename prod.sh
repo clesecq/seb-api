b3d74b8a3273c0535f081bc0a76951c13b221f61
Maxime FRIESS <maxime.friess@etu.unistra.fr>
2021-11-01 20:55:35 +0100

    [api] Fixed bug with gateway integration

--- prod.sh
+++ prod.sh
@@ -3,7 +3,7 @@
 git pull
 composer install --optimize-autoloader --no-dev
 php artisan config:cache
-php artisan route:cache
+# php artisan route:cache
 php artisan view:cache
 npm install
 npm run prod
