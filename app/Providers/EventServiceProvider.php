5d3ad2faa956ee82bf25b5f7a2b28fdd8f27c758
Charles LESECQ <charles.lesecq@etu.unistra.fr>
2024-11-11 21:40:28 +0100

    [laravel] upgrade to laravel 11

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -21,9 +21,6 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
     ];
 
     private static function getModels(): Collection
